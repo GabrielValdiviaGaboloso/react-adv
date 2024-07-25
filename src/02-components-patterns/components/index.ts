1e5bde0231e0b71f8b6bd7dff161b79eb2d1672e
Gabriel Valdivia <gabo.angel123@gmail.com>
2024-07-25 17:47:20 -0400

    component compound listo

---
+++ src/02-components-patterns/components/index.ts
@@ -0,0 +1,25 @@
+import { ProductCardHOCProps } from "../interfaces/interfaces";
+import { ProductCard as ProductCardHOC} from "./ProductCard";
+
+import { ProductButtons } from "./Productbuttons";
+import { ProductImage } from "./Productimage";
+import { ProductTitle } from "./Productitle";
+
+
+export { ProductButtons } from "./Productbuttons";
+export { ProductImage } from "./Productimage";
+export { ProductTitle } from "./Productitle";
+
+
+
+export const ProductCard: ProductCardHOCProps = Object.assign( ProductCardHOC, {
+
+  Title: ProductTitle,  
+  Image: ProductImage,
+  Buttons: ProductButtons
+
+
+
+})
+
+export default ProductCard;
