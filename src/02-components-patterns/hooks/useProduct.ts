9920a2ec92e9a1ca4d487d41d6ecd1a51f2c2147
Gabriel Valdivia <gabo.angel123@gmail.com>
2024-08-08 17:35:29 -0400

    control alternativo

--- src/02-components-patterns/hooks/useProduct.ts
+++ src/02-components-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { onChangeArgs, Product } from '../interfaces/interfaces';
 
 
@@ -17,14 +17,12 @@ export const useProduct = ({onChange,product,value= 0}:useProuctArgs) => {
     const [counter, setCounter] = useState(value);
 
    
-    const isControlled = useRef( !!onChange )
+   
     
 
     const increaseBy = (value:number) =>{
         
-        if(isControlled.current && onChange){
-            return onChange ({ count:value,product } )
-        }
+    
 
         const newValue = Math.max( counter + value , 0 )
         setCounter ( newValue)
--- src/02-components-patterns/hooks/useShoppingCart.ts
+++ src/02-components-patterns/hooks/useShoppingCart.ts
@@ -14,41 +14,28 @@ export const useShoppingCart = () =>{
     const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
   
   
-  
+      console.log({count});
+      
   
   
       setshopingCart(oldShoppinCart => {
   
-        const productInCart: ProductInCart = oldShoppinCart[product.id] || { ...product, count: 0 }
   
   
-        if (Math.max(productInCart.count + count, 0) > 0) {
-          productInCart.count += count;
-          return {
-            ...oldShoppinCart,
-            [product.id]: productInCart
+  
+          if (count === 0) {
+  
+            const { [product.id]: toDelet, ...rest } = oldShoppinCart;
+            console.log({ toDelet });
+  
+            return rest;
+  
           }
   
-        }
-        // borrar el producto
-        const { [product.id]: toDelet, ...rest } = oldShoppinCart;
-        return { ...rest };
-  
-  
-  
-        //   if (count === 0) {
-  
-        //     const { [product.id]: toDelet, ...rest } = oldShoppinCart;
-        //     console.log({ toDelet });
-  
-        //     return rest;
-  
-        //   }
-  
-        //   return {
-        //     ...oldShoppinCart,
-        //     [product.id]: { ...product, count }
-        //   }
+          return {
+            ...oldShoppinCart,
+            [product.id]: { ...product, count }
+          }
       })
     }
     
