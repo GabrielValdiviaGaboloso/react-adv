9a53b097e76b92845fefe2b427ffb65dbc94063f
Gabriel Valdivia <gabo.angel123@gmail.com>
2024-10-14 22:32:31 -0300

    Actualizar

--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -1,5 +1,5 @@
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components"
-import '../styles/custom-styles.css';
+
 import { products } from "../data/products";
 
 const product = products[0]
@@ -19,15 +19,13 @@ export const ShoppingPage = () => {
       </h1>
       <hr />
 
-
-
       <ProductCard
         key={product.id}
         product={product}
-        className="bg-dark text-white "
+       
         initialValue={{
           count:6,
-          maxCount:10,
+          //maxCount:10,
         }}
 
       >
@@ -35,20 +33,13 @@ export const ShoppingPage = () => {
           ({reset,isMaxCountReached,maxCount,increaseBy,count})=>(
 
             <>
-            <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.5)' }} />
-            <ProductTitle className="text-bold" />
-            <ProductButtons className="custom-buttons" />
-            <button onClick={reset} > reset</button>
-            <button  onClick={() => increaseBy(-2)}> -2</button>
-            {
-              (!isMaxCountReached && <button onClick={() => increaseBy(2)}> +2</button> )
-            }
+            <ProductImage  />
+            <ProductTitle />
+            <ProductButtons  />
             
-            <span  > {count}- {maxCount}</span>
             </>
           )
         }
-        
       </ProductCard  >
 
 
